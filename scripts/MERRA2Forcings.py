7906987f06ce41c87732befec159c032bc4972fd
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-01-02 15:44:05 -0500

    make time as an unlimited dimension.  When averaging, adjust the time to be the middle of the averaging window.
    Change-Id: I3daeb25b1ae469a5afccffbf22a7500f9f5efbeb

--- scripts/MERRA2Forcings.py
+++ scripts/MERRA2Forcings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys, socket
-import datetime as dt
+from datetime import datetime, timedelta
 import numpy as np
 import xarray as xr
 import pandas as pd
@@ -11,12 +11,12 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 def getMERRA2stream(cdate):
 
     # MERRA2 streams starting dates
-    d1 = dt.datetime.strptime('19800101', '%Y%m%d').date()
-    d2 = dt.datetime.strptime('19920101', '%Y%m%d').date()
-    d3 = dt.datetime.strptime('20010101', '%Y%m%d').date()
-    d4 = dt.datetime.strptime('20110101', '%Y%m%d').date()
+    d1 = datetime.strptime('19800101', '%Y%m%d').date()
+    d2 = datetime.strptime('19920101', '%Y%m%d').date()
+    d3 = datetime.strptime('20010101', '%Y%m%d').date()
+    d4 = datetime.strptime('20110101', '%Y%m%d').date()
 
-    today = dt.datetime.today().date()
+    today = datetime.today().date()
 
     if d1 <= cdate < d2:
         stream = 'MERRA2_100'
@@ -52,8 +52,12 @@ def timeAverage(da_in, freq):
     da_out.lat.attrs = da_in.lat.attrs
     da_out.lon.attrs = da_in.lon.attrs
     da_out.time.attrs = da_in.time.attrs
+    da_out['time'] = da_out.time.get_index('time') + timedelta(hours=freq/2)
+    da_out.time.encoding['units'] = "seconds since 1970-01-01 00:00:00"
+    da_out.time.encoding['calendar'] = "standard"
+    timeAttrs = ["long_name", "units", "calendar"]
+    da_out.time.attrs = {key:val for key, val in da_out.time.attrs.items() if key in timeAttrs}
     da_out[da_in.name].attrs = da_in.attrs
-
     return da_out
 
 
@@ -92,14 +96,14 @@ if __name__ == "__main__":
                             formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('--date', help='date to create MERRA2 forcings for MOM6', metavar='YYYYMMDD', type=str, required=True)
     parser.add_argument('--outdir', help='directory to save final forcings to', type=str, default=outdir, required=False)
-    parser.add_argument('--tavg_rad', help='time-averaging frequency for Radiation forcings (hours)', type=int, default=12, required=False)
+    parser.add_argument('--tavg_rad', help='time-averaging frequency for Radiation forcings (hours)', type=int, default=24, required=False)
     parser.add_argument('--tavg_met', help='time-averaging frequency for Meteorological forcings (hours)', type=int, default=6, required=False)
     parser.add_argument('--MERRA2dir', help='directory containing MERRA2 dataset', type=str, required=lMERRA2, default=dir_MERRA2)
     parser.add_argument('--tavg_pcp', help='time-averaging frequency for Precipitation forcings (hours)', type=int, default=24, required=False)
 
     args = parser.parse_args()
 
-    cdate = dt.datetime.strptime(args.date, '%Y%m%d').date()
+    cdate = datetime.strptime(args.date, '%Y%m%d').date()
     outdir = os.path.realpath(args.outdir)
     tavg_rad = args.tavg_rad
     tavg_met = args.tavg_met
@@ -150,15 +154,15 @@ if __name__ == "__main__":
     # Write to netCDF
     fstr = outdir+'/merra2.{:%Y%m%d}.'.format(cdate)
 
-    tavg_swgnt.to_netcdf(path=fstr+'SWGNT.nc4', mode='w')
-    tavg_lwgnt.to_netcdf(path=fstr+'LWGNT.nc4', mode='w')
+    tavg_swgnt.to_netcdf(path=fstr+'SWGNT.nc4', mode='w', unlimited_dims=['time'])
+    tavg_lwgnt.to_netcdf(path=fstr+'LWGNT.nc4', mode='w', unlimited_dims=['time'])
 
-    tavg_slp.to_netcdf(path=fstr+'SLP.nc4', mode='w')
-    tavg_u10m.to_netcdf(path=fstr+'U10M.nc4', mode='w')
-    tavg_v10m.to_netcdf(path=fstr+'V10M.nc4', mode='w')
-    tavg_t2m.to_netcdf(path=fstr+'T2M.nc4', mode='w')
-    tavg_qv2m.to_netcdf(path=fstr+'QV2M.nc4', mode='w')
+    tavg_slp.to_netcdf(path=fstr+'SLP.nc4', mode='w', unlimited_dims=['time'])
+    tavg_u10m.to_netcdf(path=fstr+'U10M.nc4', mode='w', unlimited_dims=['time'])
+    tavg_v10m.to_netcdf(path=fstr+'V10M.nc4', mode='w', unlimited_dims=['time'])
+    tavg_t2m.to_netcdf(path=fstr+'T2M.nc4', mode='w', unlimited_dims=['time'])
+    tavg_qv2m.to_netcdf(path=fstr+'QV2M.nc4', mode='w', unlimited_dims=['time'])
 
-    tavg_prectot.to_netcdf(path=fstr+'PRECTOT.nc4', mode='w')
+    tavg_prectot.to_netcdf(path=fstr+'PRECTOT.nc4', mode='w', unlimited_dims=['time'])
 
     sys.exit(0)
